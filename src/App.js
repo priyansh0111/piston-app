72fe980a3e0c82f92e4df75b2006d155eb3b8076
priyansh0111 <92878100+priyansh0111@users.noreply.github.com>
2023-10-28 14:57:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Topbar from './components/Topbar/Topbar.jsx';
+import Navbaract from './components/Navbaract/Navbaract.jsx'
+import Front from './components/Front/Front';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar></Topbar>
+      <Navbaract></Navbaract>
+      <Front></Front>
     </div>
   );
 }
